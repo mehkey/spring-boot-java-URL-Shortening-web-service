cc07bdbbc36a6fabff30a45a45ad63fc6ddaeb03
mehkey <87057470+mehkey@users.noreply.github.com>
2023-01-13 22:46:31 -0500

    Update URLShorteningController.java

--- src/main/java/com/mehkey/URLShortening/controllers/URLShorteningController.java
+++ src/main/java/com/mehkey/URLShortening/controllers/URLShorteningController.java
@@ -5,16 +5,21 @@ import com.mehkey.URLShortening.service.URLShorteningService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("/url")
+@RequestMapping(path="/url",
+        //consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE,
+        method = {RequestMethod.GET, RequestMethod.POST})
 public class URLShorteningController {
 
     private final Logger log = LoggerFactory.getLogger(URLShorteningController.class);
@@ -33,11 +38,19 @@ public class URLShorteningController {
 
     @PostMapping
     public ResponseEntity<URL> insertProduct(@RequestBody URL url) {
+        //URL p = service.saveURL(url);
+        //p = new URL()
+        //LocalDateTime time = LocalDateTime.now();
+        //URL p = new URL(1,"https://example.com", time, "abc123");
+
+        log.info("Saving url: " + url.toString());
         URL p = service.saveURL(url);
+
         URI location = ServletUriComponentsBuilder.fromCurrentRequestUri()
                 .path("/{id}")
                 .buildAndExpand(p.getId())
                 .toUri();
+
         return ResponseEntity.created(location).body(p);
     }
 

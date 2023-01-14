36a4cebd65ae03dc9e09f21d753bb2081ef46044
mehkey <87057470+mehkey@users.noreply.github.com>
2023-01-13 22:45:32 -0500

    Update URLShorteningControllerTest.java

--- src/test/java/com/mehkey/URLShortening/controllers/URLShorteningControllerTest.java
+++ src/test/java/com/mehkey/URLShortening/controllers/URLShorteningControllerTest.java
@@ -1,17 +1,25 @@
 package com.mehkey.URLShortening.controllers;
 
 import com.mehkey.URLShortening.entities.URL;
-import jakarta.transaction.Transactional;
+import com.mehkey.URLShortening.service.URLShorteningService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+import reactor.core.publisher.Mono;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Profile("test")
@@ -19,15 +27,19 @@ public class URLShorteningControllerTest {
     @Autowired
     private WebTestClient client;
 
-    /*@Autowired
-    private JdbcTemplate jdbcTemplate;
+    @MockBean
+    private URLShorteningService service;
 
-    private List<Integer> getIds() {
-        return jdbcTemplate.query("select id from url",
-                (rs, rowNum) -> rs.getInt("id"));
-    }*/
+
+    URLShorteningControllerTest() {
+
+    }
+
     @Test
     void URLRequest1ShouldBeOK() {
+        URL url = new URL(1,"https://example.com", LocalDateTime.now(), null);
+        when(service.findById(1)).thenReturn(Optional.of(url));
+
         client.get().uri("/url/1")
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
@@ -39,6 +51,9 @@ public class URLShorteningControllerTest {
 
     @Test
     void URLRequestAllShouldBeOK() {
+        List<URL> urls = Arrays.asList(new URL("https://example.com"), new URL("https://example2.com"));
+        when(service.getAllUrls()).thenReturn(urls);
+
         client.get().uri("/url/all")
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
@@ -46,9 +61,28 @@ public class URLShorteningControllerTest {
                 .expectHeader().contentType(MediaType.APPLICATION_JSON)
                 //.consumeWith(System.out::println);
                 .expectBodyList(URL.class)
-                .hasSize(3)
+                .hasSize(2)
                 .consumeWith(System.out::println);
     }
 
+    @Test
+    public void insertProduct_ShouldReturnCreatedProduct() {
+        LocalDateTime time = LocalDateTime.now();
+        URL url = new URL(1,"https://example.com", time, "abc124");
+        //when(service.saveURL(url)).thenReturn(new URL(1,"https://example.com", time, "abc123"));
+        ///when(service.saveURL(url)).thenReturn(url);
+        when(service.saveURL(any(URL.class))).thenReturn(url);
 
+        client.post().uri("/url")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(url))
+                .exchange()
+                .expectStatus().isCreated()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody(URL.class)
+                .consumeWith(result -> {
+                    assertEquals("https://example.com", result.getResponseBody().getUrl());
+                    assertEquals("abc124", result.getResponseBody().getShortUrl());
+                });
+    }
 }

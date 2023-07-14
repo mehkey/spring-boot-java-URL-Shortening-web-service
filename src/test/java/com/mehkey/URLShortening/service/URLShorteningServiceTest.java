d46ef1b3833f303508c682155157a59cfe73cf56
mehkey <87057470+mehkey@users.noreply.github.com>
2023-07-13 23:56:38 -0400

    Update URLShorteningServiceTest.java

--- src/test/java/com/mehkey/URLShortening/service/URLShorteningServiceTest.java
+++ src/test/java/com/mehkey/URLShortening/service/URLShorteningServiceTest.java
@@ -14,6 +14,7 @@ import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class URLShorteningServiceTest {
+
     private URLShorteningService service;
     private URLRepository dao = mock(URLRepository.class);
     private URLCacheRepository cache = mock(URLCacheRepository.class);
